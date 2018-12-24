2af3e9264dd837cdeba1f8ebe25572bd9769d8e9
Joshua Toenyes <josh@robotsnacks.io>
2018-12-24 09:22:32 -0700

    feat(lerna): also set yarn config, change [[ ... ]]

--- lerna/entrypoint.sh
+++ lerna/entrypoint.sh
@@ -2,17 +2,19 @@
 
 NPM_REGISTRY=${NPM_REGISTRY:-"//registry.npmjs.org"}
 
-if [[ ! -z "${GIT_USER_EMAIL}" ]]; then
+if [ ! -z "${GIT_USER_EMAIL}" ]; then
   git config --global user.email "$GIT_USER_EMAIL"
 fi
 
-if [[ ! -z "${GIT_USER_NAME}" ]]; then
+if [ ! -z "${GIT_USER_NAME}" ]; then
   git config --global user.name "$GIT_USER_NAME"
 fi
 
-if [[ ! -z "${NPM_TOKEN}" ]]; then
+if [ ! -z "${NPM_TOKEN}" ]; then
   npm config set ${NPM_REGISTRY}/:_authToken ${NPM_TOKEN}
   npm config set ${NPM_REGISTRY}/:always-auth true
+  yarn config set ${NPM_REGISTRY}/:_authToken ${NPM_TOKEN}
+  yarn config set ${NPM_REGISTRY}/:always-auth true
 fi
 
 sh -ec "lerna $*"
