b37d3e485934433857fd5dab3bd629bbbb6f2418
Joshua Toenyes <josh@robotsnacks.io>
2018-12-24 08:58:32 -0700

    feat(lerna): set git config values in entrypoint

--- lerna/entrypoint.sh
+++ lerna/entrypoint.sh
@@ -1,3 +1,15 @@
 #! /bin/sh
 
-sh -ec "lerna $*"
+if [[ -z "${GIT_USER_EMAIL}" ]]; then
+  echo \$GIT_USER_EMAIL must be set.
+  exit 1
+fi
+
+if [[ -z "${GIT_USER_NAME}" ]]; then
+  echo \$GIT_USER_NAME must be set.
+  exit 1
+fi
+
+git config --global user.email "$GIT_USER_EMAIL" \
+  && git config --global user.name "$GIT_USER_NAME" \
+  && sh -ec "lerna $*"
