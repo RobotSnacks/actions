88094a0b59c3dc4931ea699e85bcfb03cecc6fb2
Joshua Toenyes <josh@robotsnacks.io>
2018-12-23 20:34:17 -0700

    feat(helm): add basic helm action

---
+++ helm/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [[ -z "${GCLOUD_CLUSTER_NAME}" ]]; then
+  echo \$GCLOUD_CLUSTER_NAME must be set.
+  exit 1
+else
+
+if [[ -z "${GCLOUD_PROJECT}" ]]; then
+  echo \$GCLOUD_PROJECT must be set.
+  exit 1
+else
+
+if [[ -z "${GCLOUD_CLUSTER_ZONE}" ]]; then
+  echo \$GCLOUD_CLUSTER_ZONE must be set.
+  exit 1
+else
+
+gcloud container clusters get-credentials \
+  $GCLOUD_CLUSTER_NAME \
+  --project=$GCLOUD_PROJECT \
+  --zone=$GCLOUD_CLUSTER_ZONE
+
+sh -c "helm $*"
